3f373caacc9dae2f4b0942f4db3a1a3229f404a2
Fu Hoang <fuhoang84@googlemail.com>
2025-01-03 10:25:01 -0400

    Highlighted selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -10,9 +10,10 @@ import useGenres, { Genre } from "../hooks/useGenres";
 
 interface Props {
   onSelectedGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectedGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -29,6 +30,7 @@ const GenreList = ({ onSelectedGenre }: Props) => {
               src={genre.image_background}
             />
             <Button
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectedGenre(genre)}
               fontSize="lg"
               variant="link"
